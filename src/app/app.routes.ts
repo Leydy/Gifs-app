7f49341ef510a6beff4e27416a8c0bb558a2b08e
leydy <leydymayumy@gmail.com>
2026-03-26 16:02:31 -0500

    Tarea rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => 
+      import('./gifs/pages/dashboard-page/dashboard-page.component'),
+      
+  },
+  {
+    path: 'trending',
+    loadComponent: () => 
+      import('./gifs/pages/trending-page/trending.component/trending.component'),
+      
+  },
+  {
+    path: 'search',
+    loadComponent: () => 
+      import('./gifs/pages/search-page/search.component/search.component'),
+      
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+
+];
---
+++ src/app/gifs/pages/trending-page/trending.component/trending.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending.component',
+  imports: [],
+  templateUrl: './trending.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class TrendingPageComponent { }
