7bf23c194d079fbc95478a033fa6512d9b8ef7cf
leydy <leydymayumy@gmail.com>
2026-03-30 15:35:00 -0500

    Mostrar imágenes de relleno

--- src/app/gifs/pages/trending-page/trending.component/trending.component.ts
+++ src/app/gifs/pages/trending-page/trending.component/trending.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { GifListComponent } from "../../../components/gif-list/gif-list.component";
+import { GifListItemComponent } from "../../../components/gif-list/gif-list-item/gif-list-item.component";
 @Component({
+  standalone: true,
   selector: 'app-trending.component',
-  imports: [],
+  imports: [GifListComponent],
   templateUrl: './trending.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+
 })
 export default class TrendingPageComponent { }
