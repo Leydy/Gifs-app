f1e5cfe0da9fda34df33a78b1117bbd99d998b6a
leydy <leydymayumy@gmail.com>
2026-03-30 12:24:04 -0500

    RouterLinks-Desde componentes hijos

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 
 })
 export class SideMenuOptionsComponent {
+
+  menuOptions: MenuOption[] = [
+    {
+      label: 'Trending',
+      subLabel: 'Gifs populares',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line'
+    },
+    {
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    }
+  ];
+
 
 }
