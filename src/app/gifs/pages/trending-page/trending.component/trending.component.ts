f6f6f45c78e89421d2270186b4f0dbd1fd92f401
leydy <leydymayumy@gmail.com>
2026-03-30 15:58:21 -0500

    Input de imágenes

--- src/app/gifs/pages/trending-page/trending.component/trending.component.ts
+++ src/app/gifs/pages/trending-page/trending.component/trending.component.ts
@@ -1,6 +1,20 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { GifListComponent } from "../../../components/gif-list/gif-list.component";
-import { GifListItemComponent } from "../../../components/gif-list/gif-list-item/gif-list-item.component";
+const imageUrls: string[] = [
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg",
+    "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg"
+];
+
 @Component({
   standalone: true,
   selector: 'app-trending.component',
@@ -8,4 +22,6 @@ import { GifListItemComponent } from "../../../components/gif-list/gif-list-item
   templateUrl: './trending.component.html',
 
 })
-export default class TrendingPageComponent { }
+export default class TrendingPageComponent {
+  gifs = signal (imageUrls);
+ }
