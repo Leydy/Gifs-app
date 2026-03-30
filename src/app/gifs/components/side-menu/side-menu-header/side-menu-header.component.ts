d911ad0d513c52560ee3745b818ba607ba299a2f
leydy <leydymayumy@gmail.com>
2026-03-30 14:01:46 -0500

    Angular Environments y Path Alias

--- src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
+++ src/app/gifs/components/side-menu/side-menu-header/side-menu-header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+
 
 @Component({
   selector: 'gifs-side-menu-header',
@@ -8,5 +10,5 @@ import { Component } from '@angular/core';
 
 })
 export class SideMenuHeaderComponent {
-
+  envs = environment;
 }
